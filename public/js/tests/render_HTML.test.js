7158c9df0b7aa1c945c1d21d5a99177fafe17392
casalgado <casalgado86@yahoo.com>
2019-10-15 17:43:06 -0500

    started with mocha

--- public/js/tests/render_HTML.test.js
+++
@@ -1,8 +0,0 @@
-const { Html } = require('../render_HTML');
-
-describe('add', () => {
-	it('should add a to b', () => {
-		let localObj = new Html();
-		expect(localObj.add(2, 3)).toBe(5);
-	});
-});
